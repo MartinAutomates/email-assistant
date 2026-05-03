bc7d283b0848629ed7c6755ed620a6a19a8d9ceb
Martin Stoyanov <martin.aisetup@outlook.com>
2026-05-03 16:23:23 +0300

    add email endpint with int path parameter

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return{"message": "Hello World"}
+
+@app.get("/email/{email_id}")
+def read_email(email_id: int):
+    return{"email_id": email_id, "subject": "Test email"}
